1c08d1926e8b3092c3bc0dbb083874fbce959ae2
seraphcoin <37600902+seraphcoin@users.noreply.github.com>
2018-05-30 16:05:25 -0500

    Obfuscation Dummy address Fixed

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -163,7 +163,7 @@ public:
 
         nPoolMaxTransactions = 3;
         strSporkKey = "04a72cdafbd514beba17787899e4f97346fcf96575cc74d545145f9056d09c39492d8ddf42971bd5a3ac03947d40d7470bc0b3000fab52ebb6ca2c509a404ce324";
-        strObfuscationPoolDummyAddress = "P2MsmZwhprg6UUGnM5bLkFRXUh2Hkrma7D";
+        strObfuscationPoolDummyAddress = "P6xToMedbmhz4LdbgjaRZ5QCNYpityBznh";
         nStartMasternodePayments = 1519260063;
     }
 
