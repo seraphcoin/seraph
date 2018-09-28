527c181286eaee764ad51c0aedb5adb5721a3e25
seraphcoin <37600902+seraphcoin@users.noreply.github.com>
2018-09-27 21:00:08 -0500

    Fix: Invalid memory access on shutdown
    Fix: Invalid memory access on shutdown
    
    Problem:
    When the wallet is rescanning block for its transaction, if we close
    the wallet, the wallet will be crashed and wallet.dat will be corrupted
    in some case.
    
    The root cause is that the rescanning process is still on progress while
    the wallet is shutting down. When OS releases all allocated memory of the
    wallet and the scanning process accesses that memory, invalid memory access
    will occur => wallet.dat will be corrupted in this case

--- src/init.cpp
+++ src/init.cpp
@@ -66,7 +66,7 @@ CWallet* pwalletMain = NULL;
 int nWalletBackups = 10;
 #endif
 bool fFeeEstimatesInitialized = false;
-bool fRestartRequested = false; // true: restart false: shutdown
+std::atomic<bool> fRestartRequested(false); // true: restart false: shutdown
 
 #if ENABLE_ZMQ
 static CZMQNotificationInterface* pzmqNotificationInterface = NULL;
